e6b81924f4cac256eda2f890617ce9112c62e879
David Caron <dcaron05@gmail.com>
2018-12-18 17:30:57 -0500

    simplify and cleanup docker-compose deployment

--- pavics-compose.sh
+++ pavics-compose.sh
@@ -5,7 +5,16 @@ RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
 
 # list of all variables to be substituted in templates
-VARS='$PAVICS_FQDN $DOC_URL $MAGPIE_USER $MAGPIE_PW $MAGPIE_ADMIN_PW $SUPPORT_EMAIL'
+
+VARS='
+  $USING_PAVICS_COMPOSE
+  $HOST_FQDN
+  $POSTGIS_DB
+  $POSTGIS_USER
+  $POSTGIS_PASSWORD
+  $SSL_CERTIFICATE
+  $SSL_PRIVATE_KEY
+'
 
 # we switch to the real directory of the script, so it still works when used from $PATH
 # tip: ln -s /path/to/pavics-compose.sh ~/bin/
@@ -39,13 +48,6 @@ fi
 #   exit 1
 # fi
 
-TIMEWAIT_REUSE=$(/sbin/sysctl -n  net.ipv4.tcp_tw_reuse)
-if [[ $TIMEWAIT_REUSE -eq 0 ]]
-then
-  echo "${YELLOW}Warning:${NORMAL} the sysctl net.ipv4.tcp_tw_reuse is not enabled"
-  echo "         It it suggested to set it to 1, otherwise the pavicscrawler may fail"
-fi
-
 # we apply all the templates
 find . -name '*.template' -print0 | 
   while IFS= read -r -d $'\0' FILE
@@ -54,24 +56,7 @@ find . -name '*.template' -print0 |
     cat $FILE | envsubst "$VARS" > $DEST
   done
 
-# the PROXY_SECURE_PORT is a little trick to make the compose file invalid without the usage of this wrapper script
-PROXY_SECURE_PORT=443 HOSTNAME=$PAVICS_FQDN docker-compose $*
-ERR=$?
-
-# execute post-compose function if exists and no error occurred
-type post-compose 2>&1 | grep 'post-compose is a function' > /dev/null
-if [[ $? -eq 0 ]]
-then
-  [[ $ERR -gt 0 ]] && { echo "Error occurred with docker-compose, not running post-compose"; exit $?; }
-  post-compose $*
-fi
-
-# we restart the proxy after an up to make sure nginx continue to work if any container IP address changes
-while [[ $# -gt 0 ]]
-do
-  [[ $1 == "up" ]] && { PROXY_SECURE_PORT=443 HOSTNAME=$PAVICS_FQDN docker-compose restart proxy; }
-  shift
-done
+docker-compose $*
 
 
 
